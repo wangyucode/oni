394ed22b27ff40134c2f4132bffe3c775143921b
yu.wang <wangyu@wycode.cn>
2025-07-07 01:41:24 +0800

    refactor(页面): 重构选择器组件和样式
    - 将硬编码的选择类型改为动态渲染，使用统一的处理函数
    - 移除未使用的导入和状态变量
    - 优化选择器组件的样式结构
    - 为Selection类型添加type字段以支持分类
    - 清理data.ts中未使用的元素数据

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,20 +1,20 @@
 
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useState } from 'react';
 import { View } from '@tarojs/components'
-import { Avatar, Badge, Collapse, Grid, Tabbar, Table } from '@nutui/nutui-react-taro'
+import { Avatar, Badge, Collapse, Grid, Tabbar } from '@nutui/nutui-react-taro'
 
 import Icon, { icons } from 'src/components/icons'
 import plus from 'src/components/icons/plus.png'
 import Select from 'src/components/Select';
 import { SelectionsContext } from 'src/components/SelectionsContext';
-import { Selection } from 'src/components/data';
 
 import './index.scss'
+
+const selectionTypes = ['复制人/仿生人', '建筑', '动物', '植物']
 
 function Index() {
 
   const [select, setSelect] = useState<string>('');
-  const [dupeSelections, setDupeSelections] = useState<Array<Selection>>([]);
   const selections = useContext(SelectionsContext);
 
   function handleSwitchTab(index: number) {
@@ -23,45 +23,32 @@ function Index() {
 
   }
 
-  function handleAddDupe() {
-    setSelect('复制人/仿生人');
+  function handleAdd(type) {
+    setSelect(type);
   }
 
   function onClose() {
     setSelect('');
   }
 
-  useEffect(() => {
-    setDupeSelections(selections.filter(selection => selection.item.name === '复制人' || selection.item.name === '仿生人'));
-  }, selections)
-
   return (
     <View className='nutui-react-demo'>
-      <Collapse defaultActiveName={['dupe', 'building', "creature", "plant"]} expandIcon={<Icon width={12} height={16} name='rightArrow' />} rotate={90}>
-        <Collapse.Item title="复制人/仿生人" name='dupe'>
-          <Avatar.Group gap='-4'>
-            {dupeSelections.map(({ count, item }) =>
-              <Badge value={count}>
-                <Avatar
-                  src={icons[item.icon]}
-                  shape='square'
-                />
-              </Badge>)}
-          </Avatar.Group>
-
-          <Avatar className='avatar-add' shape='square' src={plus} onClick={handleAddDupe} style={{ marginLeft: 10, backgroundColor: '#7F3D5E' }} />
-
-        </Collapse.Item>
-        <Collapse.Item title="建筑" name="building">
-          建筑
-        </Collapse.Item>
-        <Collapse.Item title="动物" name="creature">
-          动物
-        </Collapse.Item>
-        <Collapse.Item title="植物" name="plant">
-          植物
-        </Collapse.Item>
+      <Collapse defaultActiveName={selectionTypes} expandIcon={<Icon width={12} height={16} name='rightArrow' />} rotate={90}>
+        {selectionTypes.map(type =>
+          <Collapse.Item title={type} name={type} key={type} >
+            <Avatar.Group gap='-4'>
+              {selections.filter(s => s.type === type).map(({ count, item }) =>
+                <Badge value={count} key={item.name}>
+                  <Avatar
+                    src={icons[item.icon]}
+                    shape='square'
+                  />
+                </Badge>)}
+            </Avatar.Group>
+            <Avatar className='avatar-add' shape='square' src={plus} onClick={() => handleAdd(type)} style={{ backgroundColor: '#7F3D5E' }} />
+          </Collapse.Item>)}
       </Collapse>
+
       <Collapse defaultActiveName={['resource', 'food', "power"]} expandIcon={<Icon width={12} height={16} name='rightArrow' />} rotate={90}>
         <Collapse.Item title="资源" name='resource'>
           <Grid></Grid>
