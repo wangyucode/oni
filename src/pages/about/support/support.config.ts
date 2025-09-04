5cc794b4411858b8c25cc992470dd89cab735d2d
yu.wang <wangyu@wycode.cn>
2025-09-05 02:39:07 +0800

    feat(支持页面): 添加分享功能和打赏点击事件
    - 在支持页面配置中启用分享功能
    - 添加分享按钮样式和点击处理
    - 实现打赏图片预览功能
    - 移除未使用的应用列表请求代码

--- src/pages/about/support/support.config.ts
+++ src/pages/about/support/support.config.ts
@@ -1,3 +1,4 @@
 export default definePageConfig({
   navigationBarTitleText: "支持运营",
+  enableShareAppMessage: true,
 });
--- src/pages/about/support/support.tsx
+++ src/pages/about/support/support.tsx
@@ -1,11 +1,46 @@
-import { View, Text, Image } from "@tarojs/components";
-
-import thanks from 'src/xiexie.jpg';
-import './support.scss'
+import { useEffect, useState } from "react";
+import { View, Text, Image, Button } from "@tarojs/components";
+import Taro, { useShareAppMessage } from "@tarojs/taro";
 import { Cell } from "@nutui/nutui-react-taro";
 import { ArrowRight } from "@nutui/icons-react-taro";
 
+import thanks from 'src/xiexie.jpg';
+import './support.scss'
+
 export default function Support() {
+
+    useShareAppMessage(() => ({
+        title: 'oni产物计算器',
+        path: '/pages/index/index',
+        imageUrl: 'https://oxygennotincluded.wiki.gg/zh/images/e/ea/%E8%B6%85%E7%BA%A7%E8%AE%A1%E7%AE%97%E6%9C%BA.png',
+    }));
+
+    // const [apps, setApps] = useState([]);
+
+    // useEffect(() => {
+    //     Taro.showLoading({
+    //         title: '加载中',
+    //     });
+
+    //     Taro.request({
+    //         url: 'https://wycode.cn/api/v1/wechat/apps',
+    //         method: 'GET',
+    //         success: (res) => {
+    //             console.log(res);
+    //             if (res.data.success) {
+    //                 setApps(res.data.payload);
+    //             }
+    //         },
+    //         complete: () => {
+    //             Taro.hideLoading();
+    //         }
+    //     })
+    // }, []);
+
+    function handleReward() {
+        Taro.previewImage({ urls: ["https://wycode.cn/upload/image/fish/reward.jpg"] });
+    }
+
     return (
         <View className='root'>
             <View className="header">
@@ -13,8 +48,13 @@ export default function Support() {
                 <Text className="desc">《oni产物计算器》的更新维护离不开您的支持！您可以通过以下方式支持我们。</Text>
             </View>
             <Cell.Group>
-                <Cell title="分享给好友❤️" extra={<ArrowRight />} />
-                <Cell title="打赏我" extra={<ArrowRight />} />
+                <Cell>
+                    <Button openType="share" className="share">
+                        <Text className="share-title">分享给好友❤️</Text>
+                        <ArrowRight />
+                    </Button>
+                </Cell>
+                <Cell title="打赏我" extra={<ArrowRight />} onClick={handleReward} />
                 <Cell title="意见反馈QQ群" extra={<Text selectable>1026563022</Text>} />
                 <Cell title="联系我" extra={<Text selectable>wangyu@wycode.cn</Text>} />
             </Cell.Group>
