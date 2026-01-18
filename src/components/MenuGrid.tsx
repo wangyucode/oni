e7cdd941a91ef1bda0430f4c20e2d371c3a3ccce
yu.wang <wangyu@wycode.cn>
2026-01-19 01:57:56 +0800

    style(components): 调整菜单网格和选择弹窗的样式
    - 添加 MenuGrid.scss 文件定义图标样式
    - 优化 SelectPopup 的内边距和滚动行为
    - 将 MenuGrid 图标样式从内联改为类名引用

--- src/components/MenuGrid.tsx
+++ src/components/MenuGrid.tsx
@@ -1,6 +1,9 @@
 import { Image } from "@tarojs/components";
 import { Grid } from "@nutui/nutui-react-taro";
 
+import './MenuGrid.scss';
+
+
 export type MenuGridItem = {
   name: string;
   icon?: string;
@@ -21,7 +24,7 @@ export default function MenuGrid<TItem extends MenuGridItem = MenuGridItem>({
     <Grid columns={columns} gap={0}>
       {items.map((item) => (
         <Grid.Item key={item.name} text={item.name} onClick={() => onItemClick?.(item)}>
-          {item.icon ? <Image src={item.icon} style={{ width: 64, height: 64 }} mode="aspectFit" /> : null}
+          {item.icon ? <Image src={item.icon} className="menu-grid-icon" mode="aspectFit" /> : null}
         </Grid.Item>
       ))}
     </Grid>
