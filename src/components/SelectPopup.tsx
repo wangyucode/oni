f347cf987dafc51c3132dcae4c4dd464785b2d38
yu.wang <wangyu@wycode.cn>
2026-01-24 18:58:59 +0800

    scroll & btn

--- src/components/SelectPopup.tsx
+++ src/components/SelectPopup.tsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useMemo, useState } from "react";
-import { Text, View } from "@tarojs/components";
+import { ScrollView, Text, View } from "@tarojs/components";
 import { Button, Popup } from "@nutui/nutui-react-taro";
 import { ArrowLeft } from "@nutui/icons-react-taro";
 
@@ -84,22 +84,21 @@ export default function SelectPopup({ visible, onClose }: SelectPopupProps) {
 
     return (
         <Popup
-            className="popup-root select-popup"
+            className="popup-root"
             visible={visible}
             position="bottom"
             title={title}
-            left={selectedLink || backStack.length > 1 ? <Button className="btn-back" onClick={handleGoBack}><ArrowLeft size={16} />返回</Button> : null}
+            left={selectedLink || backStack.length > 1 ? <ArrowLeft onClick={handleGoBack}/> : null}
             onClose={handleClose}
             closeable
-            style={{ maxHeight: '50%', paddingBottom: process.env.TARO_ENV === 'h5' ? 50 : 0 }}
+            style={{ marginBottom: process.env.TARO_ENV === 'h5'  ? '50px' : '0',}}
         >
-            <View className="flex-1 p-8 overflow-y-auto">
-                {content ?? (
-                    <View className="p-16">
-                        <Text>加载中...</Text>
-                    </View>
-                )}
-            </View>
+            <ScrollView style={{ height: 'calc(100% - 48px)',  }} scrollY={true}>
+                <View className="p-8">
+                    {content ?? (<Text>加载中...</Text>)}
+                </View>
+            </ScrollView>
+
         </Popup>
     );
 }
