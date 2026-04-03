cb82283d0ef93dcda6e6486fcfc0366fe67ff1ee
yu.wang <wangyu@wycode.cn>
2026-04-04 03:53:58 +0800

    v2.2.0

--- src/pages/about/log/log.tsx
+++ src/pages/about/log/log.tsx
@@ -40,6 +40,13 @@ export default function Log() {
                     </>
                 }
                 />
+                <Cell title='2.2.0' extra='2026-04-04' description={
+                    <>
+                        <Text>• 修复产物计算建筑无法同时使用多配方的问题</Text>
+                        <Text>• 新增需求计算模块，支持：氧气需求，食物需求，火箭需求计算。与产物计算互补。</Text>
+                    </>
+                }
+                />
             </Cell.Group>
             {process.env.TARO_ENV === 'weapp' && <AdCustom unitId='adunit-737af672508ba1fa' adIntervals={30} />}
         </View>
